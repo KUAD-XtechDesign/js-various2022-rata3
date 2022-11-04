589bad5f28723db035332a62122170c6784b78f0
平田莉佳 <107531993+rata3@users.noreply.github.com>
2022-11-04 16:33:13 +0900

    change

--- sample/js/script02.js
+++ sample/js/script02.js
@@ -1,5 +1,15 @@
 $(function(){
+  
+  $("#intro p").hide().fadeIn(1000)
+  //setTimeout(introAnime,2000);//2秒経った時にintroAnimeを実行
+  $("#intro").on("click",introAnime)//クリックしたらintroAnimeを実行
 
+  function introAnime(){//jqueryでフェードアウト、終わったらcontentAnime実行
+    $("#intro").fadeOut(1000,contentAnime);
+  }
+  function contentAnime(){
+    setTimeout(backAnime,300);//0.3秒後backAnime実行
+  }
   let windowH
   let documentH 
   let documentW 
