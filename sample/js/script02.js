2646a4e53926d31edd1484cbace1c173267fe5d1
平田莉佳 <107531993+rata3@users.noreply.github.com>
2022-11-08 09:34:40 +0900

    change

--- sample/js/script02.js
+++ sample/js/script02.js
@@ -2,14 +2,26 @@ $(function(){
   
   $("#intro p").hide().fadeIn(1000)
   //setTimeout(introAnime,2000);//2秒経った時にintroAnimeを実行
-  $("#intro").on("click",introAnime)//クリックしたらintroAnimeを実行
+  $("#intro").on("click",introAnime).on("click",introAnime2)//クリックしたらintroAnimeを実行
 
   function introAnime(){//jqueryでフェードアウト、終わったらcontentAnime実行
-    $("#intro").fadeOut(1000,contentAnime);
+    $("#intro").fadeOut(2000,contentAnime);
   }
   function contentAnime(){
     setTimeout(backAnime,300);//0.3秒後backAnime実行
   }
+
+  $("#intro2 p").hide().fadeIn(1000)
+  //setTimeout(introAnime,2000);//2秒経った時にintroAnimeを実行
+  $("#intro2").on("click",introAnime2).on("click",introAnime)//クリックしたらintroAnimeを実行
+
+  function introAnime2(){//jqueryでフェードアウト、終わったらcontentAnime実行
+    $("#intro2").fadeOut(1000,contentAnime2);
+  }
+  function contentAnime2(){
+    setTimeout(backAnime,300);//0.3秒後backAnime実行
+  }
+  
   let windowH
   let documentH 
   let documentW 
